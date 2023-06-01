175ac1b67f3adcd678ed071e1713384aa2c45df9
Lucas Augusto <lucas_lapada@hotmail.com>
2023-05-31 21:27:45 -0300

    Criação da pagina de Busca simples; Criação do filtro de busca; Impemnetado regra para pegar datas padrões no controllrs

--- VendasWebMvc/Controllers/RegistroDeVendasController.cs
+++ VendasWebMvc/Controllers/RegistroDeVendasController.cs
@@ -3,18 +3,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VendasWebMvc.Services;
 
 namespace VendasWebMvc.Controllers
 {
     public class RegistroDeVendasController : Controller
     {
+
+        private readonly RegistroDeVendasService _registroDeVendasService;
+        public RegistroDeVendasController(RegistroDeVendasService registroDeVendas)
+        {
+            _registroDeVendasService = registroDeVendas;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult BuscaSimples()
+        public async Task< IActionResult> BuscaSimples(DateTime? dataMinima, DateTime? dataMaxima)
         {
-            return View();
+            //isso vai mostra um valor padrão la no formulario de filtrar
+            if (!dataMinima.HasValue)
+            {
+                dataMinima =  new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!dataMaxima.HasValue)
+            {
+                dataMaxima = DateTime.Now;
+            }
+
+            ViewData["dataMinima"] = dataMinima.Value.ToString("yyyy-MM-dd");
+            ViewData["dataMaxima"] = dataMaxima.Value.ToString("yyyy-MM-dd");
+            var result = await _registroDeVendasService.EncontrarPorData(dataMinima,dataMaxima);
+            return View(result);
         }
         public IActionResult PesquisaAgrupada()
         {
---
+++ VendasWebMvc/Services/RegistroDeVendasService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMvc.Data;
+using VendasWebMvc.Models;
+
+namespace VendasWebMvc.Services
+{
+    public class RegistroDeVendasService
+    {
+        private readonly VendasWebMvcContext _context;
+
+        public RegistroDeVendasService(VendasWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task< List<RegistroDeVendas>> EncontrarPorData(DateTime? dataMin, DateTime? dataMax)
+        {
+            var result = from obj in _context.RegistroDeVendas select obj;
+            if (dataMin.HasValue)// se foi informado uma data minima
+            {
+                result = result.Where(x => x.Data >= dataMin.Value);
+            }
+
+            if (dataMax.HasValue)//se foi informado a data maxima
+            {
+                result = result.Where(x=> x.Data <= dataMax.Value);
+            }
+
+            return await result
+                .Include(x=> x.Vendedor)
+                .Include(x=> x.Vendedor.Departamento)
+                .OrderByDescending(x=> x.Data)
+                .ToListAsync();
+        }
+    }
+}
